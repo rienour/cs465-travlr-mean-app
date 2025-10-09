60ce143bfecf477e41cd2c8964283960c55104a7
Eichenour, Riley <R.D.Eichenour@gmail.com>
2025-10-09 18:54:54 -0500

    [MOD6] - Add trips data service
    Signed-off-by: Eichenour, Riley <R.D.Eichenour@gmail.com>

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -2,15 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { trips } from '../data/trips';
 import { CommonModule } from '@angular/common';
 import { TripCard } from '../trip-card/trip-card';
+import { Trip } from '../models/trip';
+import { TripData } from '../services/trip-data';
 
 @Component({
   selector: 'app-trip-listing',
   imports: [CommonModule, TripCard],
+  providers: [TripData],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
 })
 export class TripListing implements OnInit {
-  trips: Array<any> = trips;
+  trips: Array<Trip> = [];
+  message: string = '';
 
-  ngOnInit(): void {}
+  constructor(private tripDataService: TripData) {
+    console.info('trip-listing constructor');
+  }
+
+  private getData(): void {
+    this.tripDataService.getTrips().subscribe({
+      next: (value: any) => {
+        this.trips = value;
+        if (value.length > 0) {
+          this.message = 'There are ' + value.length + ' trips available.';
+        } else {
+          this.message = 'There were no trips retireved from the database';
+        }
+        console.log(this.message);
+      },
+      error: (error: any) => {
+        console.log('Error: ' + error);
+      },
+    });
+  }
+  ngOnInit(): void {
+    this.getData();
+  }
 }
